2bccbe370e5b887caed61b057d717b95fce43265
KYK <kay2kay1@gmail.com>
2017-08-25 01:17:20 +0800

    Added divisble

--- ADA_WOD_Wk02_03.js
+++ ADA_WOD_Wk02_03.js
@@ -4,21 +4,26 @@ function isPrime(){
 	if (x==2) { 																	// 2 is the only even prime number
 	//return true;
 	document.getElementById('answer').innerHTML = x + " is a Prime number!";		//locate 'answer' html element and modify
+	document.getElementById('divisible').innerHTML = '';
 	return 
 	} else if (x%2==0 || x==1) { 													// eliminate all even numbers and number 1
 	//return false;
 	document.getElementById('answer').innerHTML = x + " is not a Prime number!";
+	document.getElementById('divisible').innerHTML = x + " is divisible by 2." ;
 	return 
 	} else {   																		// starting with 2, test all numbers (i) between i and x
 	for (i=2; i<x; i++) {
 	  //if (x%i==0) return false;
 		if (x%i==0) {
+		var divis=i
 		document.getElementById('answer').innerHTML = x + " is not a Prime number!";
+		document.getElementById('divisible').innerHTML = x + " is divisible by " + i;
 		return   																	// if at any point in the loop x becomes divisible by i, end the loop and return false
 		}
 	}
 	//return true;
 	document.getElementById('answer').innerHTML = x + " is a Prime number!";
+	document.getElementById('divisible').innerHTML = '';
 	return 																			// if the loop ends without x ever being divisible by i (all numbers from 2 to x-1), 
 																					// means x is only divisible by itself and therefore is prime
   }   																				// *all numbers can be divisible by 1, so there's no need to test against 1
